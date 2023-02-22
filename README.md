93fa747de8e54b7e01397bad363de491332ba66e
Zack <zack@ukaton.com>
2023-02-22 10:28:51 -0700

    server setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pink-trombone-vvvv
- 
+
+for the security stuff, run the command in the terminal:
+`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./selfsigned.key -out selfsigned.crt`
+and put the \*.pem files in a "sec" folder
---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var https = require("https");
+var http = require("http");
+var fs = require("fs");
+var app = express();
+var WebSocket = require("ws");
+
+var options = {
+  key: fs.readFileSync("./sec/key.pem"),
+  cert: fs.readFileSync("./sec/cert.pem"),
+};
+
+app.use(express.static("public"));
+
+const httpServer = http.createServer(app);
+httpServer.listen(80);
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(443);
+
+const wss = new WebSocket.Server({ server: httpsServer });
+wss.on("connection", (ws) => {
+  console.log("new ws connection");
+  ws.on("message", (data) => {
+    console.log("ws message received");
+    // FILL
+  });
+  ws.on("close", () => {
+    console.log("ws disconnected");
+  });
+});
