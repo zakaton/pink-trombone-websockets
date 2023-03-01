72182f7d97c300fdcacff20cb03d8bd9b1b3f0bd
Zack <zack@ukaton.com>
2023-03-01 10:03:55 -0700

    Revert "removed pocketsphinx"
    This reverts commit bb2039b6e17ec71a7fe043b1e3e22ddc137f61f0.

---
+++ public/pocketsphinx/src/script.js
@@ -0,0 +1,26 @@
+/* global setupWebsocket, setupPocketSphinx */
+
+const { send } = setupWebsocket("phoneme", (message) => {
+  // FILL
+});
+
+const { startRecording, stopRecording } = setupPocketSphinx(
+  ({ hyp, hypseg }) => {
+    //console.log(hyp, hypseg);
+    let silenceLength = 0;
+    hypseg.toReversed().every(
+      (silenceLength,
+      ({ word, start, end }) => {
+        if (word == "SIL") {
+          silenceLength += end - start;
+          return true;
+        }
+      }),
+      0
+    );
+
+    if (silenceLength > 300) {
+      stopRecording();
+    }
+  }
+);
